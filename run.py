df8bbf948bbf70e93ca772fcc4c993c0e3ba0d90
Nate Baer <njbbaer@gmail.com>
2022-09-15 23:57:54 -0700

    Save completions in a log file

--- run.py
+++ run.py
@@ -1,19 +1,40 @@
 import openai
 import os
+from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import LiteralScalarString
 
 CONTEXT_FILE = 'context.txt'
+LOG_FILE = 'log.yml'
 PARAMETERS = {
     'model': 'text-davinci-002',
-    'temperature': 0,
+    'temperature': 0.0,
 }
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-with open(CONTEXT_FILE, 'r') as f:
-    text = f.read()
+yaml = YAML()
 
-response = openai.Completion.create(prompt=text, **PARAMETERS)
-text = response['choices'][0]['text']
+with open(CONTEXT_FILE, 'r') as file:
+    prompt = file.read()
 
-with open(CONTEXT_FILE, 'a') as f:
-    f.write(text)
+if os.path.exists(LOG_FILE):
+    with open(LOG_FILE, 'r') as file:
+        log = yaml.load(file.read())
+else:
+    log = []
+
+response = openai.Completion.create(prompt=prompt, **PARAMETERS)
+completion = response['choices'][0]['text']
+
+log.append({
+    'id': int(log[0]['id']) + 1 if log else 0,
+    'parameters': PARAMETERS,
+    'prompt': LiteralScalarString(prompt),
+    'completion': LiteralScalarString(completion),
+})
+
+with open(CONTEXT_FILE, 'a') as file:
+    file.write(completion)
+
+with open(LOG_FILE, 'w') as file:
+    yaml.dump(log, file)
